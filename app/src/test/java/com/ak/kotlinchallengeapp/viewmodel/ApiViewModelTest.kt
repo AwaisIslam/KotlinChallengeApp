1bebef527191d1669422ccffc7953bdcca31cb19
AwaisIslam <awais4823@gmail.com>
2022-07-19 17:48:54 +0500

    Updated Project

--- app/src/test/java/com/ak/kotlinchallengeapp/viewmodel/ApiViewModelTest.kt
+++ app/src/test/java/com/ak/kotlinchallengeapp/viewmodel/ApiViewModelTest.kt
@@ -1,13 +1,38 @@
 package com.ak.kotlinchallengeapp.viewmodel
 
-import org.junit.Assert.*
+import android.app.Application
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import com.ak.kotlinchallengeapp.repository.ApiRepository
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ApiViewModelTest{
 
     private lateinit var viewModel: ApiViewModel
+    private lateinit var repository: ApiRepository
+
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val application = Mockito.mock(Application::class.java)
 
     @Before
     fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        viewModel = ApiViewModel(repository,application)
+    }
+
+    @Test
+    fun getAllApiData() {
+        viewModel.getAllApiData()
+        assert(viewModel.response.value?.size != null)
     }
 }
